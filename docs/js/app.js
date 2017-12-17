3f6382c600e0b9f9e169ba656b99a27bd661cf13
Qrac <qrac.jp@gmail.com>
2017-12-17 20:33:02 +0900

    Fix demo

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,6 +1,20 @@
 'use strict';
 
 //----------------------------------------------------
+// Documentation Nav
+//----------------------------------------------------
+
+$(function () {
+  $('.js-doc-navs .js-doc-nav').on('click', function () {
+    if ($(this).not('is-active')) {
+      $('.js-doc-navs .js-doc-nav').removeClass('is-active');
+      $(this).addClass('is-active');
+      var index = $('.js-doc-navs .js-doc-nav').index(this);
+      $('.js-doc-contents .js-doc-content').eq(index).addClass('is-active').siblings('.js-doc-content').removeClass('is-active');
+    }
+  });
+});
+//----------------------------------------------------
 // Highlight
 //----------------------------------------------------
 
@@ -46,19 +60,6 @@ $(function () {
   if ($('.btn.is-demo-code').length) {
     $('.btn.is-demo-code').on('click', function () {
       $(this).toggleClass('is-active');
-    });
-  }
-});
-
-$(function () {
-  if ($('.group.is-documentation-nav').length) {
-    $('.group.is-documentation-nav .text.is-link-reverse').on('click', function () {
-      if ($(this).not('is-active')) {
-        $('.group.is-documentation-nav .text.is-link-reverse').removeClass('is-active');
-        $(this).addClass('is-active');
-        var index = $('.group.is-documentation-nav .text.is-link-reverse').index(this);
-        $('.groups.is-documentation-contents .content').eq(index).addClass('is-active').siblings('.content').removeClass('is-active');
-      }
     });
   }
 });
--- src/js/_toggle.js
+++ src/js/_toggle.js
@@ -9,16 +9,3 @@ $(function(){
     });
   }
 });
-
-$(function(){
-  if ($('.group.is-documentation-nav').length) {
-    $('.group.is-documentation-nav .text.is-link-reverse').on('click', function(){
-      if ($(this).not('is-active')) {
-        $('.group.is-documentation-nav .text.is-link-reverse').removeClass('is-active');
-        $(this).addClass('is-active');
-        const index = $('.group.is-documentation-nav .text.is-link-reverse').index(this);
-        $('.groups.is-documentation-contents .content').eq(index).addClass('is-active').siblings('.content').removeClass('is-active');
-      }
-    });
-  }
-});
