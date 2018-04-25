46dbb1dde4e952e6fe010b370e6d12a9aa05297b
Qrac <qrac.jp@gmail.com>
2018-04-25 10:27:35 +0900

    Remove pug setting

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -83,13 +83,5 @@ module.exports = {
         }
       ]
     }
-  },
-  chainWebpack: config => {
-    config.module
-      .rule('pug')
-      .test(/\.pug$/)
-      .use('pug-plain-loader')
-        .loader('pug-plain-loader')
-        .end()
   }
 }
