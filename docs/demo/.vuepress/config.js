20cbadde8042ab1cbfb934ead10be8ae2c1564dc
Qrac <qrac.jp@gmail.com>
2018-04-28 13:59:18 +0900

    Add stylesheet

--- docs/demo/.vuepress/config.js
+++ docs/demo/.vuepress/config.js
@@ -12,6 +12,8 @@ module.exports = {
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover' }],
     ['meta', { name: 'format-detection', content: 'telephone=no, email=no, address=no' }],
     ['meta', { name: 'theme-color', content: '#1cb4c7' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
     ['meta', { name: 'twitter:creator', content: 'Qrac_JP' }],
     ['meta', { property: 'og:title', content: 'MUSUBii' }],
@@ -20,8 +22,15 @@ module.exports = {
     ['meta', { property: 'og:image', content: 'https://qrac.github.io/musubii/img/ogp.png' }],
     ['meta', { property: 'og:site_name', content: 'Qrac GitHub' }],
     ['meta', { property: 'og:type', content: 'website' }],
+    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/yakuhanjp@3.0.0/dist/css/yakuhanrp_s.min.css' }],
+    ['link', { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.0.1/css/all.css' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito:400,700,900' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css' }],
+    ['link', { rel: 'stylesheet', href: '/css/musubii.min.css' }],
+    ['link', { rel: 'stylesheet', href: '/css/musubii-demo.min.css' }],
     ['link', { rel: 'apple-touch-icon', href: '/img/apple-touch-icon.png' }],
-    ['link', { rel: 'icon', href: '/img/favicon.png' }]
+    ['link', { rel: 'shortcut icon', href: '/img/favicon.png' }]
   ],
   serviceWorker: true,
   themeConfig: {
