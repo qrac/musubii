9f90631c835528b06f3982e4ea056f5973fb08d2
Qrac <qrac.jp@gmail.com>
2018-04-29 11:38:40 +0900

    Fix navlinks

--- docs/demo/.vuepress/theme/NavLinks.vue
+++ docs/demo/.vuepress/theme/NavLinks.vue
@@ -21,113 +21,131 @@
 </template>
 
 <script>
-import OutboundLink from './OutboundLink.vue'
-import DropdownLink from './DropdownLink.vue'
-import { resolveNavLinkItem } from './util'
-import NavLink from './NavLink.vue'
+import OutboundLink from "./OutboundLink.vue";
+import DropdownLink from "./DropdownLink.vue";
+import { resolveNavLinkItem } from "./util";
+import NavLink from "./NavLink.vue";
 
 export default {
   components: { OutboundLink, NavLink, DropdownLink },
   computed: {
-    userNav () {
-      return this.$themeLocaleConfig.nav || this.$site.themeConfig.nav || []
+    userNav() {
+      return this.$themeLocaleConfig.nav || this.$site.themeConfig.nav || [];
     },
-    nav () {
-      const { locales } = this.$site
+    nav() {
+      const { locales } = this.$site;
       if (locales && Object.keys(locales).length > 1) {
-        let currentLink = this.$page.path
-        const routes = this.$router.options.routes
-        const themeLocales = this.$site.themeConfig.locales || {}
+        let currentLink = this.$page.path;
+        const routes = this.$router.options.routes;
+        const themeLocales = this.$site.themeConfig.locales || {};
         const languageDropdown = {
-          text: this.$themeLocaleConfig.selectText || 'Languages',
+          text: this.$themeLocaleConfig.selectText || "Languages",
           items: Object.keys(locales).map(path => {
-            const locale = locales[path]
-            const text = themeLocales[path] && themeLocales[path].label || locale.lang
-            let link
+            const locale = locales[path];
+            const text =
+              (themeLocales[path] && themeLocales[path].label) || locale.lang;
+            let link;
             // Stay on the current page
             if (locale.lang === this.$lang) {
-              link = currentLink
+              link = currentLink;
             } else {
               // Try to stay on the same page
-              link = currentLink.replace(this.$localeConfig.path, path)
+              link = currentLink.replace(this.$localeConfig.path, path);
               // fallback to homepage
               if (!routes.some(route => route.path === link)) {
-                link = path
+                link = path;
               }
             }
-            return { text, link }
+            return { text, link };
           })
-        }
-        return [...this.userNav, languageDropdown]
+        };
+        return [...this.userNav, languageDropdown];
       }
-      return this.userNav
+      return this.userNav;
     },
-    userLinks () {
-      return (this.nav || []).map((link => {
+    userLinks() {
+      return (this.nav || []).map(link => {
         return Object.assign(resolveNavLinkItem(link), {
           items: (link.items || []).map(resolveNavLinkItem)
-        })
-      }))
+        });
+      });
     },
-    repoLink () {
-      const { repo } = this.$site.themeConfig
+    repoLink() {
+      const { repo } = this.$site.themeConfig;
       if (repo) {
-        return /^https?:/.test(repo)
-          ? repo
-          : `https://github.com/${repo}`
+        return /^https?:/.test(repo) ? repo : `https://github.com/${repo}`;
       }
     },
-    repoLabel () {
-      if (!this.repoLink) return
+    repoLabel() {
+      if (!this.repoLink) return;
       if (this.$site.themeConfig.repoLabel) {
-        return this.$site.themeConfig.repoLabel
+        return this.$site.themeConfig.repoLabel;
       }
 
-      const repoHost = this.repoLink.match(/^https?:\/\/[^/]+/)[0]
-      const platforms = ['GitHub', 'GitLab', 'Bitbucket']
+      const repoHost = this.repoLink.match(/^https?:\/\/[^/]+/)[0];
+      const platforms = ["GitHub", "GitLab", "Bitbucket"];
       for (let i = 0; i < platforms.length; i++) {
-        const platform = platforms[i]
-        if (new RegExp(platform, 'i').test(repoHost)) {
-          return platform
+        const platform = platforms[i];
+        if (new RegExp(platform, "i").test(repoHost)) {
+          return platform;
         }
       }
 
-      return 'Source'
-    },
+      return "Source";
+    }
   }
-}
+};
 </script>
 
-<style lang="stylus">
-@import './styles/config.styl'
+<style lang="scss">
+@import "./styles/_config.scss";
+@import "~moftone/src/scss/tone/_moftone.scss";
+@import "~sass-dashi/src/scss/_dashi.scss";
 
-.nav-links
-  display inline-block
-  a
-    line-height 1.4rem
-    color inherit
-    &:hover, &.router-link-active
-      color $accentColor
-  .nav-item
-    cursor: pointer
-    position relative
-    display inline-block
-    margin-left 1.5rem
-    line-height 2rem
-  .repo-link
-    margin-left 1.5rem
+.nav-links {
+  display: inline-block;
+  a {
+    line-height: 1.4rem;
+    color: inherit;
+    &:hover,
+    &.router-link-active {
+      color: $accentColor;
+    }
+  }
+  .nav-item {
+    cursor: pointer;
+    position: relative;
+    display: inline-block;
+    margin-left: 1.5rem;
+    line-height: 2rem;
+  }
+  .repo-link {
+    margin-left: 1.5rem;
+  }
+}
 
-@media (max-width: $MQMobile)
-  .nav-links
-    .nav-item, .repo-link
-      margin-left 0
+@media (max-width: $MQMobile) {
+  .nav-links {
+    .nav-item,
+    .repo-link {
+      margin-left: 0;
+    }
+  }
+}
 
-@media (min-width: $MQMobile)
-  .nav-links a
-    &:hover, &.router-link-active
-      color $textColor
-  .nav-item > a
-    &:hover, &.router-link-active
-      margin-bottom -2px
-      border-bottom 2px solid lighten($accentColor, 8%)
+@media (min-width: $MQMobile) {
+  .nav-links a {
+    &:hover,
+    &.router-link-active {
+      color: $textColor;
+    }
+  }
+  .nav-item > a {
+    &:hover,
+    &.router-link-active {
+      margin-bottom: -2px;
+      border-bottom: 2px solid lighten($accentColor, 8%);
+    }
+  }
+}
 </style>
