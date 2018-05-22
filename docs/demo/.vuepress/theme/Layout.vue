5b54f7a2d0ae43ef8d896b4873b87e97775c93d6
Qrac <qrac.jp@gmail.com>
2018-05-23 05:47:00 +0900

    vuepress v0.9.0

--- docs/demo/.vuepress/theme/Layout.vue
+++ docs/demo/.vuepress/theme/Layout.vue
@@ -21,157 +21,135 @@
 </template>
 
 <script>
-import Vue from "vue";
-import nprogress from "nprogress";
-import Home from "./Home.vue";
-import Navbar from "./Navbar.vue";
-import Page from "./Page.vue";
-import Sidebar from "./Sidebar.vue";
-import { pathToComponentName } from "@app/util";
-import { resolveSidebarItems } from "./util";
-
+import Vue from 'vue'
+import nprogress from 'nprogress'
+import Home from './Home.vue'
+import Navbar from './Navbar.vue'
+import Page from './Page.vue'
+import Sidebar from './Sidebar.vue'
+import { pathToComponentName } from '@app/util'
+import store from '@app/store'
+import { resolveSidebarItems } from './util'
+import throttle from 'lodash.throttle'
 export default {
   components: { Home, Page, Sidebar, Navbar },
-  data() {
+  data () {
     return {
       isSidebarOpen: false
-    };
+    }
   },
-
   computed: {
-    shouldShowNavbar() {
-      const { themeConfig } = this.$site;
-      const { frontmatter } = this.$page;
-      if (frontmatter.navbar === false) return false;
+    shouldShowNavbar () {
+      const { themeConfig } = this.$site
+      const { frontmatter } = this.$page
+      if (
+        frontmatter.navbar === false ||
+        themeConfig.navbar === false) {
+        return false
+      }
       return (
         this.$title ||
         themeConfig.logo ||
         themeConfig.repo ||
         themeConfig.nav ||
         this.$themeLocaleConfig.nav
-      );
+      )
     },
-    shouldShowSidebar() {
-      const { themeConfig } = this.$site;
-      const { frontmatter } = this.$page;
+    shouldShowSidebar () {
+      const { frontmatter } = this.$page
       return (
         !frontmatter.layout &&
         !frontmatter.home &&
         frontmatter.sidebar !== false &&
         this.sidebarItems.length
-      );
+      )
     },
-    sidebarItems() {
+    sidebarItems () {
       return resolveSidebarItems(
         this.$page,
         this.$route,
         this.$site,
         this.$localePath
-      );
+      )
     },
-    pageClasses() {
-      const userPageClass = this.$page.frontmatter.pageClass;
+    pageClasses () {
+      const userPageClass = this.$page.frontmatter.pageClass
       return [
         {
-          "no-navbar": !this.shouldShowNavbar,
-          "sidebar-open": this.isSidebarOpen,
-          "no-sidebar": !this.shouldShowSidebar
+          'no-navbar': !this.shouldShowNavbar,
+          'sidebar-open': this.isSidebarOpen,
+          'no-sidebar': !this.shouldShowSidebar
         },
         userPageClass
-      ];
+      ]
     }
   },
-
-  created() {
-    if (this.$ssrContext) {
-      this.$ssrContext.title = this.$title;
-      this.$ssrContext.lang = this.$lang;
-      this.$ssrContext.description =
-        this.$page.description || this.$description;
-    }
-  },
-
-  mounted() {
-    // update title / meta tags
-    this.currentMetaTags = [];
-    const updateMeta = () => {
-      document.title = this.$title;
-      document.documentElement.lang = this.$lang;
-      const meta = [
-        {
-          name: "description",
-          content: this.$description
-        },
-        ...(this.$page.frontmatter.meta || [])
-      ];
-      this.currentMetaTags = updateMetaTags(meta, this.currentMetaTags);
-    };
-    this.$watch("$page", updateMeta);
-    updateMeta();
-
+  mounted () {
+    window.addEventListener('scroll', this.onScroll)
     // configure progress bar
-    nprogress.configure({ showSpinner: false });
-
+    nprogress.configure({ showSpinner: false })
     this.$router.beforeEach((to, from, next) => {
-      if (
-        to.path !== from.path &&
-        !Vue.component(pathToComponentName(to.path))
-      ) {
-        nprogress.start();
+      if (to.path !== from.path && !Vue.component(pathToComponentName(to.path))) {
+        nprogress.start()
       }
-      next();
-    });
-
+      next()
+    })
     this.$router.afterEach(() => {
-      nprogress.done();
-      this.isSidebarOpen = false;
-    });
+      nprogress.done()
+      this.isSidebarOpen = false
+    })
   },
-
-  beforeDestroy() {
-    updateMetaTags(null, this.currentMetaTags);
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.onScroll)
   },
-
   methods: {
-    toggleSidebar(to) {
-      this.isSidebarOpen = typeof to === "boolean" ? to : !this.isSidebarOpen;
+    toggleSidebar (to) {
+      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
     },
     // side swipe
-    onTouchStart(e) {
+    onTouchStart (e) {
       this.touchStart = {
         x: e.changedTouches[0].clientX,
         y: e.changedTouches[0].clientY
-      };
+      }
     },
-    onTouchEnd(e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x;
-      const dy = e.changedTouches[0].clientY - this.touchStart.y;
+    onTouchEnd (e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x
+      const dy = e.changedTouches[0].clientY - this.touchStart.y
       if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
         if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true);
+          this.toggleSidebar(true)
         } else {
-          this.toggleSidebar(false);
+          this.toggleSidebar(false)
+        }
+      }
+    },
+    onScroll: throttle(function () {
+      this.setActiveHash()
+    }, 300),
+    setActiveHash () {
+      const sidebarLinks = [].slice.call(document.querySelectorAll('.sidebar-link'))
+      const anchors = [].slice.call(document.querySelectorAll('.header-anchor'))
+        .filter(anchor => sidebarLinks.some(sidebarLink => sidebarLink.hash === anchor.hash))
+      const scrollTop = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop)
+      for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i]
+        const nextAnchor = anchors[i + 1]
+        const isActive = i === 0 && scrollTop === 0 ||
+          (scrollTop >= anchor.parentElement.offsetTop + 10 &&
+            (!nextAnchor || scrollTop < nextAnchor.parentElement.offsetTop - 10))
+        if (isActive && decodeURIComponent(this.$route.hash) !== decodeURIComponent(anchor.hash)) {
+          store.disableScrollBehavior = true
+          this.$router.replace(decodeURIComponent(anchor.hash), () => {
+            // execute after scrollBehavior handler.
+            this.$nextTick(() => {
+              store.disableScrollBehavior = false
+            })
+          })
+          return
         }
       }
     }
-  }
-};
-
-function updateMetaTags(meta, current) {
-  if (current) {
-    current.forEach(c => {
-      document.head.removeChild(c);
-    });
-  }
-  if (meta) {
-    return meta.map(m => {
-      const tag = document.createElement("meta");
-      Object.keys(m).forEach(key => {
-        tag.setAttribute(key, m[key]);
-      });
-      document.head.appendChild(tag);
-      return tag;
-    });
   }
 }
 </script>
