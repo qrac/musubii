f86ad9270417dc5adf0eb5148898bc63d5a954b6
Qrac <qrac.jp@gmail.com>
2018-11-06 16:47:19 +0900

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,54 +2,64 @@
 // Gulp > npx gulp
 //----------------------------------------------------
 
-const gulp = require('gulp');
-const notify = require('gulp-notify');
-const plumber = require('gulp-plumber');
-const rename = require('gulp-rename');
-const sass = require('gulp-sass');
-const sassGlob = require('gulp-sass-glob');
-const autoprefixer = require('gulp-autoprefixer');
-const gcmq = require('gulp-group-css-media-queries');
-const cleanCSS = require('gulp-clean-css');
-const packageImporter = require('node-sass-package-importer');
+const gulp = require("gulp")
+const notify = require("gulp-notify")
+const plumber = require("gulp-plumber")
+const rename = require("gulp-rename")
+const sass = require("gulp-sass")
+const sassGlob = require("gulp-sass-glob")
+const autoprefixer = require("gulp-autoprefixer")
+const gcmq = require("gulp-group-css-media-queries")
+const cleanCSS = require("gulp-clean-css")
+const packageImporter = require("node-sass-package-importer")
 
 // Setting : Paths
 const paths = {
-  'src_scss': './src/scss/',
-  'dist_css': './docs/css/'
+  src_scss: "./src/scss/",
+  dist_css: "./docs/css/"
 }
 
 // Setting : Sass Options
 const sassOptions = {
-  outputStyle: 'expanded',
+  outputStyle: "expanded",
   importer: packageImporter({
-    extensions: ['.scss', '.css']
+    extensions: [".scss", ".css"]
   })
 }
 
 // SCSS > CSS
-gulp.task('scss', () => {
-  return gulp.src(paths.src_scss + '**/*.scss')
+gulp.task("scss", () => {
+  return gulp
+    .src(paths.src_scss + "**/*.scss")
     .pipe(sassGlob())
-    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(
+      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
+    )
     .pipe(sass(sassOptions))
-    .pipe(autoprefixer(['> 3% in JP', 'ie 11', 'android 4.4', 'last 1 versions']))
+    .pipe(
+      autoprefixer(["> 3% in JP", "ie 11", "android 4.4", "last 1 versions"])
+    )
     .pipe(gcmq())
     .pipe(gulp.dest(paths.dist_css))
-});
+})
 
 // CSS Minify
-gulp.task('cssmin', () => {
-  return gulp.src([paths.dist_css + '**/*.css', '!' + paths.dist_css + '**/*.min.css'])
-    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+gulp.task("cssmin", () => {
+  return gulp
+    .src([paths.dist_css + "**/*.css", "!" + paths.dist_css + "**/*.min.css"])
+    .pipe(
+      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
+    )
     .pipe(cleanCSS())
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(gulp.dest(paths.dist_css))
-});
+})
 
 // Watch
-gulp.task('watch', () => {
-  gulp.watch(paths.src_scss + '**/*.scss', gulp.series('scss', 'cssmin'));
-});
+gulp.task("watch", () => {
+  gulp.watch(paths.src_scss + "**/*.scss", gulp.series("scss", "cssmin"))
+})
 
-gulp.task('default', gulp.parallel('watch'));
+gulp.task("default", gulp.parallel("watch"))
+
+gulp.task("build", gulp.parallel(gulp.series("scss", "cssmin")))
