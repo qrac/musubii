20de85df8fb7c5d96b16b2ef06b81c2a2d825e31
Qrac <qrac.jp@gmail.com>
2019-10-24 18:26:52 +0900

    Print code

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,11 @@
 const path = require("path")
 
-module.exports = async ({ config, mode }) => {
-  config.module.rules.push(
-    {
-      test: /\.scss$/,
-      loaders: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../")
-    },
-    {
-      test: /\.html$/,
-      loaders: ["extract-loader", "html-loader"],
-      include: path.resolve(__dirname, "../")
-    }
-  )
+module.exports = async ({ config }) => {
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ["style-loader", "css-loader", "sass-loader"],
+    include: path.resolve(__dirname, "../")
+  })
 
   return config
 }
--- src/stories/module/button.stories.js
+++ src/stories/module/button.stories.js
@@ -1,35 +1,44 @@
-import { storiesOf } from "@storybook/html"
 import {
   withKnobs,
   boolean,
   optionsKnob as options
 } from "@storybook/addon-knobs"
+import _escape from "lodash/escape"
 
-storiesOf("Module", module)
-  .addDecorator(withKnobs)
-  .add("Button", () => {
-    const buttons = [
-      { role: "", text: "戻る" },
-      { role: " is-primary", text: "決定" },
-      { role: " is-info", text: "情報" },
-      { role: " is-success", text: "登録" },
-      { role: " is-warning", text: "注意" },
-      { role: " is-danger", text: "削除" }
-    ]
-    const inlineRadio = { display: "inline-radio" }
-    const styles = {
-      Plain: " is-plain",
-      Outline: " is-outline",
-      Melt: " is-melt"
-    }
-    const style = options("Style", styles, " is-plain", inlineRadio)
-    const round = boolean("Round", false) ? " is-round" : ""
-    const floating = boolean("Floating", false) ? " is-floating" : ""
-    const disabled = boolean("Disabled", false) ? " disabled" : ""
-    return buttons
-      .map(
-        button =>
-          `<button class="button${style}${button.role}${round}${floating}" type="button"${disabled}>${button.text}</button>`
-      )
-      .join(" ")
-  })
+export default {
+  title: "Modules",
+  decorators: [withKnobs]
+}
+
+export const modulesButton = () => {
+  const inlineRadio = { display: "inline-radio" }
+  const styles = {
+    Plain: " is-plain",
+    Outline: " is-outline",
+    Melt: " is-melt"
+  }
+  const style = options("Style", styles, " is-plain", inlineRadio)
+  const round = boolean("Round", false) ? " is-round" : ""
+  const floating = boolean("Floating", false) ? " is-floating" : ""
+  const disabled = boolean("Disabled", false) ? " disabled" : ""
+  const items = [
+    { role: "", text: "戻る" },
+    { role: " is-primary", text: "決定" },
+    { role: " is-info", text: "情報" },
+    { role: " is-success", text: "登録" },
+    { role: " is-warning", text: "注意" },
+    { role: " is-danger", text: "削除" }
+  ]
+  const buttons = items
+    .map(
+      item =>
+        `<button class="button${style}${item.role}${round}${floating}" type="button"${disabled}>${item.text}</button>`
+    )
+    .join("\n")
+  const wrapper = `<div class="box is-flex is-space-row-xs is-space-column-xs">${buttons}</div>`
+  const container = `<div class="box is-space-xs">${wrapper}<pre class="pre"><code class="code">${_escape(
+    buttons
+  )}</code></pre></div>`
+  return container
+}
+modulesButton.story = { name: "Button" }
