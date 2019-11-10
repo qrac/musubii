68afe3df926130dd678c327e54992fe6cbe69a8b
Qrac <qrac.jp@gmail.com>
2019-11-10 11:38:47 +0900

    Add text demo monotone

--- .storybook/stories/styles/components/text.stories.js
+++ .storybook/stories/styles/components/text.stories.js
@@ -24,6 +24,21 @@ const items = [
   { id: 5, role: "is-danger", text: "禁止する" }
 ]
 
+const monotoneItems = [
+  { id: 0, suffix: "" },
+  { id: 1, suffix: "-1" },
+  { id: 2, suffix: "-2" },
+  { id: 3, suffix: "-3" },
+  { id: 4, suffix: "-4" },
+  { id: 5, suffix: "-5" },
+  { id: 6, suffix: "-6" },
+  { id: 7, suffix: "-7" },
+  { id: 8, suffix: "-8" },
+  { id: 9, suffix: "-9" }
+]
+
+const tones = { dark: "is-dark", light: "is-light" }
+
 const alignStyles = {
   none: "",
   left: "is-left",
@@ -89,3 +104,47 @@ export const basic = () => {
 }
 
 basic.story = { name: "Basic" }
+
+//----------------------------------------------------
+// / Monotone
+//----------------------------------------------------
+
+export const monotone = () => {
+  const tone = radios("Tone", tones, "is-dark")
+  const toneText = tone === "is-dark" ? "ダーク" : "ライト"
+  const toneWrap = tone === "is-dark" ? "demo-line-box" : "demo-paint-box"
+  const strong = boolean("Strong", false) ? "is-strong" : ""
+  const align = radios("Align", alignStyles, "")
+  const tagBefore = align != "" ? `<p class="text ${align}">` : ""
+  const tagAfter = align != "" ? `</p>` : ""
+  const contents = monotoneItems
+    .map(
+      item =>
+        `<span class="text ${tone}${item.suffix} ${strong}"
+        >${toneText}</span>`
+    )
+    .join("")
+  const contentsWrap = (tagBefore + contents + tagAfter)
+    .replace(/\s+/g, " ")
+    .replace(/\s\"/g, '"')
+  const formatted = beautify.html(contentsWrap, beautifyHtmlOptions)
+  const highlighted = copyCodeBlock(formatted, { lang: "html" })
+  return (
+    <div className="demo-space-box">
+      <h1 className="demo-h1">Text</h1>
+      <h2 className="demo-h2">Monotone</h2>
+      <div className={toneWrap}>
+        <div
+          className="box"
+          dangerouslySetInnerHTML={{ __html: formatted }}
+        ></div>
+      </div>
+      <div
+        className="box"
+        dangerouslySetInnerHTML={{ __html: highlighted }}
+      ></div>
+    </div>
+  )
+}
+
+monotone.story = { name: "Monotone" }
