a8f382e1319d5b4836fd1682695e9f7bcebbf4e9
Qrac <qrac.jp@gmail.com>
2019-12-13 19:01:12 +0900

    Demo head

--- src/components/common/app-head.js
+++ src/components/common/app-head.js
@@ -8,6 +8,48 @@ export default ({ meta }) => {
       <title key="title">
         {meta.title ? meta.title + " - " + pjt.site.title : pjt.site.title}
       </title>
+      <meta
+        key="viewport"
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+      />
+      <meta
+        property="format-detection"
+        content="telephone=no, email=no, address=no"
+      />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta
+        key="description"
+        property="description"
+        content={pjt.site.description}
+      />
+      <meta
+        property="og:title"
+        content={
+          meta.title ? meta.title + " - " + pjt.site.title : pjt.site.title
+        }
+      />
+      <meta
+        property="og:description"
+        content={meta.description ? meta.description : pjt.site.description}
+      />
+      <meta
+        property="og:url"
+        content={meta.path ? pjt.site.url + meta.path : pjt.site.url}
+      />
+      <meta
+        property="og:image"
+        content={pjt.site.url + "/apple-touch-icon.png"}
+      />
+      <meta property="og:site_name" content={pjt.site.title} />
+      <meta
+        property="og:type"
+        content={meta.path === "/" ? "website" : "article"}
+      />
+      <meta name="twitter:card" content="summary" />
+      <meta property="twitter:creator" content={"@" + pjt.site.twitter_id} />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="icon" href="/favicon.png" />
     </Head>
   )
 }
