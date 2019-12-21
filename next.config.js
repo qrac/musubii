2f49666eacaf9ac96b1b7fc314e031a2efbf72cc
Qrac <qrac.jp@gmail.com>
2019-12-21 16:51:57 +0900

    Test demo mdx

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
-const withCSS = require("@zeit/next-css")
-const withSass = require("@zeit/next-sass")
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/
-})
+module.exports = () => {
+  //const frontmatter = require("remark-frontmatter")
 
-module.exports = withCSS(
-  withSass(
-    withMDX({
-      pageExtensions: ["js", "jsx", "mdx", "md"]
-    })
+  const withCSS = require("@zeit/next-css")
+  const withSass = require("@zeit/next-sass")
+  const withMDX = require("@next/mdx")({
+    extension: /\.mdx?$/,
+    options: {
+      //remarkPlugins: [frontmatter]
+    }
+  })
+
+  return withCSS(
+    withSass(
+      withMDX({
+        pageExtensions: ["js", "jsx", "mdx", "md"]
+      })
+    )
   )
-)
+}
