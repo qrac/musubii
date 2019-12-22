f17bba4872eb32ba2e8053d0a80707443680d3de
Qrac <qrac.jp@gmail.com>
2019-12-22 20:13:57 +0900

    Demo toc style

--- src/components/parts/demo-link.js
+++ src/components/parts/demo-link.js
@@ -1,32 +1,52 @@
 import Link from "next/link"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
+import {
+  faExternalLinkAlt,
+  faAngleDown
+} from "@fortawesome/free-solid-svg-icons"
 
 export default ({ children, props, href }) => {
   const regHttp = /^http/
+  const regToc = /^#/
   return (
     <>
-      {regHttp.test(href) ? (
-        <a
-          className="demo-link"
-          href={href}
-          rel="noopener noreferrer"
-          target="_blank"
-          {...props}
-        >
-          <span>{children}</span>
-          <FontAwesomeIcon
-            icon={faExternalLinkAlt}
-            className="icon is-external-link-fix is-sm"
-          />
-        </a>
-      ) : (
-        <Link href={href}>
-          <a className="demo-link" {...props}>
-            <span>{children}</span>
-          </a>
-        </Link>
-      )}
+      {(() => {
+        if (regHttp.test(href)) {
+          return (
+            <a
+              className="demo-link"
+              href={href}
+              rel="noopener noreferrer"
+              target="_blank"
+              {...props}
+            >
+              <span>{children}</span>
+              <FontAwesomeIcon
+                icon={faExternalLinkAlt}
+                className="icon is-external-link-fix is-sm"
+              />
+            </a>
+          )
+        } else if (regToc.test(href)) {
+          return (
+            <a className="demo-link" href={href} {...props}>
+              <span>{children}</span>
+              <FontAwesomeIcon
+                icon={faAngleDown}
+                className="icon is-angle-down-fix"
+              />
+            </a>
+          )
+        } else {
+          return (
+            <Link href={href}>
+              <a className="demo-link" {...props}>
+                <span>{children}</span>
+              </a>
+            </Link>
+          )
+        }
+      })()}
     </>
   )
 }
