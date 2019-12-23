241739729021da227d399a40769a8af05cc8cdf8
Qrac <qrac.jp@gmail.com>
2019-12-23 21:19:55 +0900

    Demo custom block

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
 module.exports = () => {
   const slug = require("remark-slug")
   const toc = require("remark-toc")
+  const customBlocks = require("remark-custom-blocks")
+
+  const customBlocksTemplate = {
+    tips: {
+      classes: "demo-box is-tips",
+      title: "optional"
+    },
+    warning: {
+      classes: "demo-box is-warning",
+      title: "optional"
+    }
+  }
 
   const withCSS = require("@zeit/next-css")
   const withSass = require("@zeit/next-sass")
   const withMDX = require("@next/mdx")({
     extension: /\.mdx?$/,
     options: {
-      remarkPlugins: [slug, toc]
+      remarkPlugins: [slug, toc, [customBlocks, customBlocksTemplate]]
     }
   })
 
