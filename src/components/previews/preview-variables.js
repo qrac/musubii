298563fa2c045634ff4bddc6ef6f48c7fbab28dd
Qrac <qrac.jp@gmail.com>
2019-12-27 23:00:15 +0900

    Demo extract replace

--- src/components/previews/preview-variables.js
+++ src/components/previews/preview-variables.js
@@ -1,3 +1,8 @@
+import test from "../../assets/extracts/mode/simple/styles/elements/_wysiwyg.json"
+
 export default ({ filePath }) => {
-  return <div></div>
+  console.log(test)
+  console.log(test.variables[2].value)
+  console.log(test.variables[2].compiledValue)
+  return <div>{test.variables[2].compiledValue}</div>
 }
