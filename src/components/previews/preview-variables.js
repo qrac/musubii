bfb79601f6a7e7a0aa7084f5e883c5da2fcfddb4
Qrac <qrac.jp@gmail.com>
2019-12-28 00:02:09 +0900

    Demo variables table

--- src/components/previews/preview-variables.js
+++ src/components/previews/preview-variables.js
@@ -1,8 +1,55 @@
-import test from "../../assets/extracts/mode/simple/styles/elements/_wysiwyg.json"
+import React from "react"
 
-export default ({ filePath }) => {
-  console.log(test)
-  console.log(test.variables[2].value)
-  console.log(test.variables[2].compiledValue)
-  return <div>{test.variables[2].compiledValue}</div>
+class PreviewVariables extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mode: "simple",
+      simpleData: require("~/assets/extracts/mode/simple/" +
+        this.props.filePath +
+        ".json"),
+      lightData: require("~/assets/extracts/mode/light/" +
+        this.props.filePath +
+        ".json"),
+      darkData: require("~/assets/extracts/mode/dark/" +
+        this.props.filePath +
+        ".json")
+    }
+  }
+  render() {
+    //console.log(this.state.mode)
+    //console.log(this.state.simpleData)
+    //console.log(this.state.lightData)
+    //console.log(this.state.darkData)
+    return (
+      <div className="demo-table-wrap">
+        <table className="demo-table">
+          <thead>
+            <tr>
+              <th>variable</th>
+              <th>value</th>
+              <th>compiledValue</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.simpleData.variables.map(variable => (
+              <tr key={variable.name}>
+                <td>
+                  <code className="code">{variable.name}</code>
+                </td>
+                <td>
+                  <code className="code">{variable.value}</code>
+                </td>
+                <td>
+                  <code className="code">{variable.compiledValue}</code>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
+
+export default PreviewVariables
