65fdbe352a5b0cce927206b5086d362a5a4736d7
Qrac <qrac.jp@gmail.com>
2019-12-31 18:42:34 +0900

    Fix demo for ie11

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,17 +5,22 @@ import { config } from "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 config.autoAddCss = false
 
-import cssVars from "css-vars-ponyfill"
-
 import AppHeader from "~/components/commons/app-header"
 import AppFooter from "~/components/commons/app-footer"
 import AppActions from "~/components/commons/app-actions"
 import LayoutDefault from "~/components/commons/layout-default"
 import LayoutDocs from "~/components/commons/layout-docs"
-import "~/assets/scss/project.scss"
 
 class MyApp extends App {
   componentDidMount() {
+    const userAgent = window.navigator.userAgent.toLowerCase()
+
+    if (userAgent.indexOf("msie") != -1 || userAgent.indexOf("trident") != -1) {
+      require("~/assets/scss/project-ie.scss")
+    } else {
+      require("~/assets/scss/project.scss")
+    }
+
     const isLocal = localStorage.getItem("theme")
     const isDark = window.matchMedia("(prefers-color-scheme: dark)")
 
@@ -26,8 +31,6 @@ class MyApp extends App {
     } else {
       document.body.setAttribute("data-theme", "light")
     }
-
-    cssVars({ exclude: "[data-theme]" })
   }
   render() {
     const { Component, pageProps, router } = this.props
