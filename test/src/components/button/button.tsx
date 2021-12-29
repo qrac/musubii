9c1880722b1e7c0d700ecea8c20b2461eb06eecf
Qrac <qrac.jp@gmail.com>
2021-12-29 16:52:12 +0900

    Support button dom elements #293

--- test/src/components/button/button.tsx
+++ test/src/components/button/button.tsx
@@ -1,18 +1,41 @@
 export interface ButtonProps {
+  DOMElement?: "button" | "a"
+  type?: "button" | "reset" | "submit"
   variant: "solid" | "outline" | "ghost"
   color?: "primary" | "secondary" | "success" | "danger"
-  text?: string
+  href?: string
+  target?: "_self" | "_blank"
   isDisabled?: boolean
+  text?: string
 }
 
 export const Button = (props: ButtonProps) => {
-  const { variant, color, text, isDisabled } = props
+  const {
+    DOMElement = "button",
+    type = "button",
+    variant,
+    color,
+    href = "",
+    target,
+    isDisabled,
+    text,
+  } = props
   const classNames = ["button", `is-${variant}`, color && `is-${color}`].join(
     " "
   )
+  const ariaDisabled = isDisabled ? true : undefined
+  const tabIndex = isDisabled ? -1 : undefined
   return (
-    <button type="button" className={classNames} disabled={isDisabled}>
+    <DOMElement
+      className={classNames}
+      type={DOMElement === "button" ? type : undefined}
+      href={DOMElement === "a" ? href : undefined}
+      target={DOMElement === "a" ? target : undefined}
+      disabled={DOMElement === "button" ? isDisabled : undefined}
+      aria-disabled={DOMElement !== "button" ? ariaDisabled : undefined}
+      tabIndex={DOMElement !== "button" ? tabIndex : undefined}
+    >
       {text}
-    </button>
+    </DOMElement>
   )
 }
--- test/src/scenes/scene-button.tsx
+++ test/src/scenes/scene-button.tsx
@@ -3,8 +3,14 @@ import { useState } from "react"
 import { DemoRadios, DemoCheckbox, Button, ButtonProps } from "../components"
 
 export const SceneButton = () => {
+  const domElements = ["button", "a"]
+  const [domElement, setDomElement] =
+    useState<ButtonProps["DOMElement"]>("button")
   const variants = ["solid", "outline", "ghost"]
   const [variant, setVariant] = useState<ButtonProps["variant"]>("solid")
+  const targets = ["_self", "_blank"]
+  const [target, setTarget] = useState<ButtonProps["target"]>("_self")
+  const targetAttr = target === "_blank" ? target : undefined
   const [isDisabled, setIsDisabled] = useState<boolean>(false)
   return (
     <div className="demo-contents">
@@ -18,6 +24,22 @@ export const SceneButton = () => {
               setVariant(event.target.value as ButtonProps["variant"])
             }
           />
+          <DemoRadios
+            items={domElements}
+            checkedItem={domElement}
+            name="domElement"
+            onChange={(event) =>
+              setDomElement(event.target.value as ButtonProps["DOMElement"])
+            }
+          />
+          <DemoRadios
+            items={targets}
+            checkedItem={target}
+            name="target"
+            onChange={(event) =>
+              setTarget(event.target.value as ButtonProps["target"])
+            }
+          />
           <DemoCheckbox
             value="disabled"
             isChecked={isDisabled}
@@ -27,30 +49,44 @@ export const SceneButton = () => {
       </div>
       <div className="demo-content">
         <div className="demo-buttons">
-          <Button variant={variant} text="戻る" isDisabled={isDisabled} />
           <Button
+            DOMElement={domElement}
+            variant={variant}
+            target={targetAttr}
+            isDisabled={isDisabled}
+            text="戻る"
+          />
+          <Button
+            DOMElement={domElement}
             variant={variant}
             color="primary"
-            text="決定"
+            target={targetAttr}
             isDisabled={isDisabled}
+            text="決定"
           />
           <Button
+            DOMElement={domElement}
             variant={variant}
             color="secondary"
-            text="変更"
+            target={targetAttr}
             isDisabled={isDisabled}
+            text="変更"
           />
           <Button
+            DOMElement={domElement}
             variant={variant}
             color="success"
-            text="登録"
+            target={targetAttr}
             isDisabled={isDisabled}
+            text="登録"
           />
           <Button
+            DOMElement={domElement}
             variant={variant}
             color="danger"
-            text="削除"
+            target={targetAttr}
             isDisabled={isDisabled}
+            text="削除"
           />
         </div>
       </div>
