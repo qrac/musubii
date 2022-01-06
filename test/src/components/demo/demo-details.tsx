13de6aa35460f09ea530f06a24fe6a4f855fb75a
Qrac <qrac.jp@gmail.com>
2022-01-06 21:09:38 +0900

    Fix demo details #293

--- test/src/components/demo/demo-details.tsx
+++ test/src/components/demo/demo-details.tsx
@@ -9,9 +9,15 @@ export interface DemoDetailsProps {
 export const DemoDetails = (props: DemoDetailsProps) => {
   const { defaultOpen = false, summary, children } = props
   const [open, setOpen] = useState(defaultOpen)
+  const onToggleOpen = (
+    event: React.MouseEvent<HTMLDetailsElement, MouseEvent>
+  ) => {
+    event.preventDefault()
+    setOpen(!open)
+  }
   return (
     <details className="demo-details" open={open}>
-      <summary className="demo-details-summary" onClick={() => setOpen(!open)}>
+      <summary className="demo-details-summary" onClick={onToggleOpen}>
         {summary}
       </summary>
       <div className="demo-details-body">{children}</div>
