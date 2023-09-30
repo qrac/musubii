42e4114ca223a293d8a09b489bfd8cd911453163
Qrac <qrac.jp@gmail.com>
2023-09-30 10:39:11 +0900

    Add playground #307

---
+++ playground/_default/src/app.tsx
@@ -0,0 +1,15 @@
+//import { useState, useEffect } from "react"
+//import queryString from "query-string"
+
+import "musubii/src/musubii.css"
+
+export default function () {
+  return (
+    <>
+      <header>
+        <h1>MUSUBii</h1>
+      </header>
+      <main></main>
+    </>
+  )
+}
