19e963e0c4d9aaeb5d2ca85986605ad505070be8
Qrac <qrac.jp@gmail.com>
2024-04-01 10:15:10 +0900

    Fix section

--- playground/_default/src/examples/section.tsx
+++ playground/_default/src/examples/section.tsx
@@ -1,12 +1,12 @@
-import { Section } from "../components/section"
-
 export function ExampleSection() {
   return (
     <div className="demo-boxes">
       <div className="demo-box">
-        <Section>
-          <div className="demo-card">{"section > inner"}</div>
-        </Section>
+        <section className="section">
+          <div className="inner">
+            <div className="demo-card">{"section > inner"}</div>
+          </div>
+        </section>
       </div>
     </div>
   )
