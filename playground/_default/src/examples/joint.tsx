c733102919990c870f33e6a32df47f6a5a61219f
Qrac <qrac.jp@gmail.com>
2024-04-02 08:47:21 +0900

    Fix joint

--- playground/_default/src/examples/joint.tsx
+++ playground/_default/src/examples/joint.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react"
 import clsx from "clsx"
-import { FiAlignLeft, FiAlignCenter, FiAlignRight } from "react-icons/fi"
+import {
+  FiAlignLeft,
+  FiAlignCenter,
+  FiAlignRight,
+  FiSearch,
+} from "react-icons/fi"
 
 import { DemoFieldsets, DemoFieldset } from "../demos/fieldset"
 import { DemoCheckbox } from "../demos/checkbox"
@@ -21,35 +26,55 @@ export function ExampleJoint() {
         </DemoFieldsets>
       </div>
       <div className="demo-box">
-        <div className="demo-buttons">
-          <div className={clsx(isActive && "joint")}>
-            <button className="button is-plain">
-              <FiAlignLeft />
-              <span className="text">左揃え</span>
-            </button>
-            <button className="button is-plain">
-              <FiAlignCenter />
-              <span className="text">中央揃え</span>
-            </button>
-            <button className="button is-plain">
-              <FiAlignRight />
-              <span className="text">右揃え</span>
-            </button>
+        <div className={clsx(isActive && "joint")}>
+          <button className="button is-plain">
+            <FiAlignLeft />
+            <span className="text">左揃え</span>
+          </button>
+          <button className="button is-plain">
+            <FiAlignCenter />
+            <span className="text">中央揃え</span>
+          </button>
+          <button className="button is-plain">
+            <FiAlignRight />
+            <span className="text">右揃え</span>
+          </button>
+        </div>
+      </div>
+      <div className="demo-box">
+        <div className={clsx(isActive && "joint")}>
+          <button className="button is-outline">
+            <FiAlignLeft />
+            <span className="text">左揃え</span>
+          </button>
+          <button className="button is-outline">
+            <FiAlignCenter />
+            <span className="text">中央揃え</span>
+          </button>
+          <button className="button is-outline">
+            <FiAlignRight />
+            <span className="text">右揃え</span>
+          </button>
+        </div>
+      </div>
+      <div className="demo-box">
+        <div className={clsx(isActive && "joint")}>
+          <div className="select">
+            <select>
+              <option>Select A</option>
+              <option>Select B</option>
+              <option>Select C</option>
+            </select>
           </div>
-          <div className={clsx(isActive && "joint")}>
-            <button className="button is-outline">
-              <FiAlignLeft />
-              <span className="text">左揃え</span>
-            </button>
-            <button className="button is-outline">
-              <FiAlignCenter />
-              <span className="text">中央揃え</span>
-            </button>
-            <button className="button is-outline">
-              <FiAlignRight />
-              <span className="text">右揃え</span>
-            </button>
-          </div>
+          <input
+            type="text"
+            className="input"
+            name="text"
+            placeholder="キーワード"
+          />
+          <button type="button" className="button is-plain is-info is-square">
+            <FiSearch />
+          </button>
         </div>
       </div>
     </div>
