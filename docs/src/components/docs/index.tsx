458251c6782b526790c5d13b3d08432353b12af2
Qrac <qrac.jp@gmail.com>
2024-04-20 22:23:59 +0900

    Fix docs

--- docs/src/components/docs/index.tsx
+++ docs/src/components/docs/index.tsx
@@ -1,3 +1,19 @@
+import { FiExternalLink } from "react-icons/fi"
+
+export function ExternalLink({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) {
+  return (
+    <a href={href} rel="noopener noreferrer" target="_blank">
+      {children} <FiExternalLink className="icon is-dark-4" />
+    </a>
+  )
+}
+
 export function Docs({ children }: { children: React.ReactNode }) {
   return <div className="docs">{children}</div>
 }
