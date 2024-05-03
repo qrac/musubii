e28047a4344604e6aa3589acff6f32aa3f0a3dd3
Qrac <qrac.jp@gmail.com>
2024-05-03 18:34:41 +0900

    Add rehype-pretty-code for docs

--- docs/minista.config.ts
+++ docs/minista.config.ts
@@ -9,22 +9,35 @@ import remarkDirective from "remark-directive"
 import remarkDirectiveRehype from "remark-directive-rehype"
 import rehypeSlug from "rehype-slug"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypePrettyCode from "rehype-pretty-code"
 
 const remarkTocOptions = {
   maxDepth: 3,
+}
+const rehypePrettyCodeOptions = {
+  theme: {
+    light: "github-light",
+    dark: "dracula-soft",
+  },
+  keepBackground: false,
 }
 
 export default defineConfig({
   plugins: [
     pluginSsg(),
     pluginMdx({
+      useRehypeHighlight: false,
       mdxOptions: {
         remarkPlugins: [
           [remarkToc, remarkTocOptions],
           remarkDirective,
           remarkDirectiveRehype,
         ],
-        rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+        rehypePlugins: [
+          rehypeSlug,
+          rehypeAutolinkHeadings,
+          [rehypePrettyCode, rehypePrettyCodeOptions],
+        ],
       },
     }),
     pluginEntry(),
