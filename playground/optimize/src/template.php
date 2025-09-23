a59c8013c849342b97d795c2889ea5f588e2c2b1
Qrac <qrac.jp@gmail.com>
2025-09-23 09:45:22 +0900

    Remove optimize #323

--- playground/optimize/src/template.php
+++
@@ -1,6 +0,0 @@
-<?php get_template_part('components/article-title'); ?>
-<div class="demo">
-  <div class="box is-padding-top-md">
-    <div class="box is-margin-md"><?php the_title(); ?></div>
-  </div>
-</div>
--- playground/optimize/src/template.tsx
+++
@@ -1,9 +0,0 @@
-export function Template() {
-  return (
-    <div className="demo">
-      <div className="box is-padding-top-md">
-        <div className="box is-margin-md">{"template"}</div>
-      </div>
-    </div>
-  )
-}
